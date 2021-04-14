3af264bdeefde1760ec3090604bd0d2901f680b3
BartMoura <bart.emf@hotmail.com>
2021-04-14 14:48:26 -0300

    validarCadastro

--- routes/usuariosRouter.js
+++ routes/usuariosRouter.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const usuariosController = require('../controllers/usuariosController')
+const validarCadastro = require("../middlewares/ValidarCadastro");
 
 router.get('/', usuariosController.index);
-router.post('/', usuariosController.create);
+router.post('/', validarCadastro, usuariosController.create);
 router.put('/:id', usuariosController.update);
 router.delete('/:id', usuariosController.delete);
-//router.get('/perfil/:id', usuariosController.show);
-
 
 module.exports = router;
